9832071886fdbf90eaa1e7eab3b49680ce37c5b1
antoine-coulon <coulon_antoine@outlook.fr>
2022-01-24 21:18:06 +0100

    fix: add conditional spinner rendering based on the tty

--- src/reporters/reporter.ts
+++ src/reporters/reporter.ts
@@ -18,31 +18,57 @@ export type Reporter = {
  * reporting either in console or in a .html file.
  * @param logger
  */
-export function reportScannerLoggerEvents(logger: Logger) {
-  const spinner = new Spinner({
-    text: consolePrinter.util.concatOutputs([
-      consolePrinter.font.highlight("@nodesecure/scanner").bold().underline()
-        .message,
-      consolePrinter.font.standard("Analysis started").bold().message
-    ]).message
-  });
+export function reportScannerLoggerEvents(logger: Logger): void {
+  const startMessage = consolePrinter.util.concatOutputs([
+    consolePrinter.font.highlight("@nodesecure/scanner").bold().underline()
+      .message,
+    consolePrinter.font.standard("Analysis started").bold().message
+  ]);
+  /**
+   * The Spinner internally requires a TTY terminal (i.e. when Node.js
+   * detects that it is being run with a text terminal ("TTY") attached, the
+   * process.stdout.isTTY is "true").
+   * In the context of CI pipelines, this program could be run without a TTY.
+   * see: https://nodejs.org/api/tty.html
+   */
+  let spinner: Spinner;
+  if (process.stdout.isTTY) {
+    spinner = new Spinner({
+      text: startMessage.message
+    });
+  }
 
   logger.once("start", () => {
+    if (!spinner) {
+      startMessage.print();
+
+      return;
+    }
     spinner.start();
   });
 
   logger.once(ScannerLoggerEvents.done, () => {
-    const { elapsedTime } = spinner;
     const endMessage = consolePrinter.util.concatOutputs([
       consolePrinter.font.highlight("@nodesecure/scanner").bold().underline()
         .message,
-      consolePrinter.font.standard("Analysis ended").bold().message,
+      consolePrinter.font.standard("Analysis ended").bold().message
+    ]);
+
+    if (!spinner) {
+      endMessage.print();
+
+      return;
+    }
+
+    const { elapsedTime } = spinner;
+    const endMessageWithElapsedTime = consolePrinter.util.concatOutputs([
+      endMessage.message,
       consolePrinter.font
         .info(`${ms(elapsedTime)}`)
         .italic()
         .bold().message
     ]).message;
 
-    spinner.succeed(endMessage);
+    spinner.succeed(endMessageWithElapsedTime);
   });
 }
