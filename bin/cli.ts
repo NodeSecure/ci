4e1a4de7f5b0df9c9bb11458837681895efb946d
antoine-coulon <coulon_antoine@outlook.fr>
2022-03-15 01:08:10 +0100

    feat: add cli command to initialize the .nodesecurerc file

--- bin/cli.ts
+++ bin/cli.ts
@@ -2,6 +2,7 @@
 
 import sade from "sade";
 
+import { generateDefaultNodeSecureConfig } from "../src/configuration/index.js";
 import { Nsci } from "../src/configuration/standard/index.js";
 import { runPipeline } from "../src/reporting/run.js";
 
@@ -14,43 +15,49 @@ const availableWarnings = joinExclusiveList(Nsci.warnings);
 const availableStrategies = joinExclusiveList(Nsci.vulnStrategy);
 const availableReporters = joinExclusiveList(Nsci.reporterTarget);
 
-const program = sade("nsci", true);
+const program = sade("nsci");
 
 program
+  .command("init")
+  .example("cli.js init")
+  .action(generateDefaultNodeSecureConfig);
+
+program
+  .command("run")
   .option(
     "-d, --directory",
     "Root directory from which the analyses will be run.",
     process.cwd()
   )
-  .example("cli.js --directory=/Users/user1/myproject")
+  .example("cli.js run --directory=/Users/user1/myproject")
 
   .option(
     "-s, --strategy",
     `@nodesecure/vuln vulnerability strategy. Can be '${availableStrategies}'`,
     Nsci.vulnStrategy.npm
   )
-  .example("cli.js --strategy=npm")
+  .example("cli.js run --strategy=npm")
 
   .option(
     "-v, --vulnerabilities",
     `Vulnerability severity threshold. Can be '${availableVulnThresholds})'`,
     Nsci.vulnSeverity.MEDIUM
   )
-  .example("cli.js --vulnerabilities=medium")
+  .example("cli.js run --vulnerabilities=medium")
 
   .option(
     "-w, --warnings",
     `Action when detecting warnings. Can be '${availableWarnings}'`,
     Nsci.warnings.ERROR
   )
-  .example("cli.js --warnings=off")
+  .example("cli.js run --warnings=off")
 
   .option(
     "-r, --reporters",
     `Pipeline reporters. Can be '${availableReporters}'`,
     Nsci.reporterTarget.CONSOLE
   )
-  .example("cli.js --reporters=console,html")
+  .example("cli.js run --reporters=console,html")
 
   .describe(
     "Run @nodesecure pipeline checks to hunt vulnerabilities for both source code and dependencies"
